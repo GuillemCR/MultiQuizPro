684317cf6e03d6a131fcfec586e5c44206f12424
Guillem CoRa <gcomasr@gmail.com>
2017-10-06 09:23:36 +0200

    Comença MultiQuizPro

---
+++ app/src/main/java/edu/upc/eseiaat/pma/guillemcomas/multiquizpro/MultiQuizActivity.java
@@ -0,0 +1,13 @@
+package edu.upc.eseiaat.pma.guillemcomas.multiquizpro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MultiQuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multi_quiz);
+    }
+}
